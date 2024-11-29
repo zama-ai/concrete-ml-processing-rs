22673cd899c38d19c191c66c3434efe68ab1d80d
jfrery <jordan.frery@zama.ai>
2024-11-29 10:33:13 +0100

    chore: fix path

--- scripts/generate_logistic_regression_quantizer_params.py
+++ scripts/generate_logistic_regression_quantizer_params.py
@@ -2,23 +2,49 @@ from concrete.ml.sklearn import LogisticRegression
 from sklearn.datasets import load_iris
 import json
 import numpy as np
+from pathlib import Path
 
 # Load data
 X_train, y_train = load_iris(return_X_y=True)
+
+print("Shape of X_train:", X_train.shape)
+print("Shape of y_train:", y_train.shape)
 
 # Train model
 model = LogisticRegression()
 model.fit(X_train, y_train)
 
+# Extract quantized model parameters
+q_weights = model._q_weights
+q_bias = model._q_bias
+
+# Get script directory
+script_dir = Path(__file__).parent
+
+# Save weights and bias
+(script_dir / "q_weights.json").write_text(
+    json.dumps(q_weights.tolist())
+)
+
+(script_dir / "q_bias.json").write_text(
+    json.dumps(q_bias.tolist())
+)
+
 # Save input quantization parameters
-with open("input_quantizer.json", "w") as f:
+with (script_dir / "input_quantizer.json").open("w") as f:
     for quantizer in model.input_quantizers:
         quantizer.dump(f)
 
 # Save output quantization parameters
-with open("output_quantizer.json", "w") as f:
+with (script_dir / "output_quantizer.json").open("w") as f:
     for quantizer in model.output_quantizers:
         quantizer.dump(f)
+
+# Save weights quantization parameters
+with (script_dir / "weights_quantizer.json").open("w") as f:
+    f.write(
+        json.dumps(model._weight_quantizer.dump(f))
+)
 
 # Generate test values and predictions
 float_test_samples = X_train[0:5]  # Taking first 5 samples as test data
@@ -33,12 +59,13 @@ quantized_predictions = model._inference(quantized_test_samples)
 dequantized_predictions = model.dequantize_output(quantized_predictions)
 
 test_data = {
-    "input_fp32_values": quantized_test_samples.tolist(),
+    "input_fp32_values": float_test_samples.tolist(),
     "input_quantized_values": quantized_test_samples.tolist(),
     "quantized_predictions": quantized_predictions.tolist(),
     "dequantized_predictions": dequantized_predictions.tolist()
 }
 
 # Save test values and predictions
-with open("test_values.json", "w") as f:
-    json.dump(test_data, f, indent=2)
+(script_dir / "test_values.json").write_text(
+    json.dumps(test_data, indent=2)
+)
